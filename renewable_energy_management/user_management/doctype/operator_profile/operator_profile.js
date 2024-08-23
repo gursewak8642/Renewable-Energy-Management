f48b9b8c82f3af14f41946bb526318ffe48e06b7
gursewak8642 <gursewak8642@gmail.com>
2024-08-23 11:02:23 +0530

    first commit

---
+++ renewable_energy_management/user_management/doctype/operator_profile/operator_profile.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Team and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Operator Profile", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ renewable_energy_management/user_management/doctype/operator_profile/test_operator_profile.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Team and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOperatorProfile(FrappeTestCase):
+	pass
